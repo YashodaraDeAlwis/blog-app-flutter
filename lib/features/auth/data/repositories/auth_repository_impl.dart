8b16921113da06438c93c216c1a917b3e1e785dc
Yashodara <yashdealwis@gmail.com>
2024-10-22 08:19:18 +0530

    add: signup auth

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,8 +1,11 @@
 import 'package:blog/core/error/failures.dart';
+import 'package:blog/features/auth/data/data_sources/auth_remote_data_source.dart';
 import 'package:blog/features/auth/domain/repository/i_auth_repository.dart';
 import 'package:fpdart/src/either.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
+  AuthRepositoryImpl(AuthRemoteDataSource authRemoteDataSource);
+
   @override
   Future<Either<Failures, String>> loginWithEmailPassword(
       {required String email, required String password}) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:blog/core/secrets/app_secret.dart';
 import 'package:blog/core/theme/theme.dart';
+import 'package:blog/features/auth/data/data_sources/auth_remote_data_source.dart';
+import 'package:blog/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog/features/auth/domain/use_cases/user_sign_up.dart';
+import 'package:blog/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // just do this as a precausion
   final supabase = await Supabase.initialize(
-      url: AppSecret.superbaseUrl, anonKey: AppSecret.superbaseAnonKey);
-  runApp(const MyApp());
+    url: AppSecrets.superbaseUrl,
+    anonKey: AppSecrets.superbaseAnonKey,
+  );
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (_) => AuthBloc(
+          userSignUp: UserSignUp(
+            AuthRepositoryImpl(
+              AuthRemoteDataSourceImpl(
+                supabase.client,
+              ),
+            ),
+          ),
+        ),
+      )
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
